83e1d36c089ab775bc6df95489e1f5eb6fc19c94
Jacques <jacques@swipeix.com>
2025-07-13 23:13:31 +0200

    Refactor cover component to update CONFIG_SCHEMA definition (`cover.COVER_SCHEMA` is deprecated and will be removed in ESPHome 2025.11.0)

--- esphome/components/dc_blue/cover.py
+++ esphome/components/dc_blue/cover.py
@@ -1,20 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import cover, binary_sensor
-from esphome.components.dc_blue import DcBlueComponent
+from esphome.components import cover
+from esphome.components.dc_blue import dc_blue_component_ns, DcBlueComponent
 from esphome.components.dc_blue.constants import (
     CONF_DC_BLUE_ID,
-)
-from esphome.const import (
-    CONF_ID,
 )
 
 import esphome.config_validation as cv
 
 DEPENDENCIES = ["dc_blue", "cover"]
 
+DcBlueCover = dc_blue_component_ns.class_("DcBlueCover", cover.Cover, cg.Component)
 
-CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
+CONFIG_SCHEMA = cover.cover_schema(DcBlueCover).extend(
     {
         cv.GenerateID(CONF_DC_BLUE_ID): cv.use_id(DcBlueComponent),
     }
@@ -25,6 +23,4 @@ async def to_code(config):
     platform = await cg.get_variable(config[CONF_DC_BLUE_ID])
     
     sens = platform.create_garage_cover_sensor()
-    # sens = cg.new_Pvariable(config[CONF_ID])
     await cover.register_cover(sens, config)
-    # cg.add(platform.set_garage_cover_sensor(sens))
